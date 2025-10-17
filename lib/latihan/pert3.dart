28ffa8d0d2b01bf6ae1395077734b176663020fe
abimanyupewe <abimanyupw369@gmail.com>
2025-10-17 14:47:43 +0700

    tugas per1

---
+++ lib/latihan/pert3.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Pert3 extends StatefulWidget {
+  @override
+  State<Pert3> createState() => _Pert3State();
+}
+
+class _Pert3State extends State<Pert3> {
+  var nama = "Abimanyu";
+  var jurusan = "TI";
+  var nim = 23201245;
+
+  TextEditingController pesanController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pertemuan 3", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Nama : ${nama}"),
+                    Text("Jurusan : ${jurusan}"),
+                    Text("Nim : ${nim.toString()}"),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            TextField(
+              controller: pesanController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hint: Text("data"),
+                prefixIcon: Icon(Icons.edit, size: 20),
+              ),
+            ),
+            SizedBox(height: 50,),
+            ElevatedButton(onPressed: () => {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar( content: Text(pesanController.text)))
+            },style: ButtonStyle(), child: Text("Tekan Aku"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tugas/pertemuan1.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Pertemuan1 extends StatelessWidget {
+  final String nama = 'Abimanyu Priyo Widagdo';
+  final List<String> hobi = ['Bersepeda', 'Membaca', 'Coding'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pertemuan 1", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(fontSize: 16, color: Colors.black45),
+                        children: [
+                          TextSpan(text: "Halo Saya "),
+                          TextSpan(
+                            text: nama,
+                            style: TextStyle(color: Colors.purple),
+                          ),
+                          TextSpan(
+                            text:
+                                ", Biasa dipanggil Abim. Saya memiliki beberapa hobi ada ",
+                          ),
+                          TextSpan(
+                            text: hobi.join(', '),
+                            style: TextStyle(color: Colors.purple),
+                          ),
+                          TextSpan(text: "."),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
